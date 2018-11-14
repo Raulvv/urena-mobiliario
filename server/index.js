1a009e304f4b9dd7edae3c227bef70d782d1674d
Raul Vega del Valle <raulvv@Rauls-MacBook-Pro.local>
2018-11-14 16:00:14 +0100

    setting up email mailer config

--- server/index.js
+++ server/index.js
@@ -15,6 +15,19 @@ app.get('/api', function (req, res) {
   res.send('{"message":"Hello from the custom server!"}');
 });
 
+app.post("/api/send_email", function(req, res) {
+  res.set("Content-Type", "application/json");
+
+  const locals = { userName: req.body.userName };
+  const messageInfo = {
+    email: req.body.email, fromEmail: "raul.vega.dv@gmail.com",
+    fromName: "Star Wars", subject: "Checkout this awesome droids"
+  };
+  mailer.sendOne("droids", messageInfo, locals);
+
+  res.send('{"message":"Email sent."}');
+}
+
 // All remaining requests return the React app, so it can handle routing.
 app.get('/', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
