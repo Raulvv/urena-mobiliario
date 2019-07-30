3bcfef88374405576f3d244ea63244a383119e63
Raul Vega <rvr_sge@mango.com>
2019-07-30 16:01:18 +0200

    Mailer class on same file than server

--- api/index.js
+++ api/index.js
@@ -1,16 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+class Mailer {
+  constructor() {
+    const creds = {
+      USER: 'raul.vega.dv@gmail.com',
+      PASS: 'gudvhlqnlrexajoi'
+    };
+
+    const transport = {
+      host: 'smtp.gmail.com',
+      auth: {
+        user: creds.USER,
+        pass: creds.PASS
+      }
+    };
+
+    this.transporter = nodemailer.createTransport(transport);
+  }
+
+  sendEmail(mail) {
+    this.transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+      }
+
+      this.transporter.sendMail(mail, (err, data) => {
+        console.log('sending...');
+        if (err) {
+          res.json({
+            msg: 'fail'
+          })
+        }
+
+        res.json({
+          msg: 'success'
+        });
+      })
+      console.log('Server is ready to take messages');
+    });
+  }
+}
+
 const app = express();
+const mailer = new Mailer();
 
 app.use(bodyParser.json());
 
 app.post('/api/mailer', function(req, res) {
+  console.log("INSIDE");
   const name = req.body.name
   const company = req.body.company
   const phone = req.body.phone
   const email = req.body.email
   const message = req.body.message
   const content = `name: ${name} \n email: ${email} \n message: ${message} \n company: ${company} \n phone: ${phone} `
+  console.log("After getting variables", content);
 
   var mail = {
     from: name,
@@ -18,8 +64,10 @@ app.post('/api/mailer', function(req, res) {
     subject: 'New Message from Contact Form',
     text: content
   }
-  console.log("Mailer", Mailer);
-  // new Mailer().sendEmail(mail);
+  console.log("After setting mail", mail);
+
+  mailer.sendEmail(mail);
+  console.log("GETTING OUT");
 });
 
 
