3fb4d7637003b27615298b72204c086230a312e0
Raul Vega <rvr_sge@mango.com>
2019-07-30 16:43:13 +0200

    Adding mailer as a separate file

--- api/index.js
+++ api/index.js
@@ -1,50 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-
-class Mailer {
-  constructor() {
-    const creds = {
-      USER: 'raul.vega.dv@gmail.com',
-      PASS: 'gudvhlqnlrexajoi'
-    };
-
-    const transport = {
-      host: 'smtp.gmail.com',
-      auth: {
-        user: creds.USER,
-        pass: creds.PASS
-      }
-    };
-
-    this.transporter = nodemailer.createTransport(transport);
-  }
-
-  sendEmail(mail) {
-    this.transporter.verify((error, success) => {
-      if (error) {
-        console.log(error);
-      }
-
-      this.transporter.sendMail(mail, (err, data) => {
-        console.log('sending...');
-        if (err) {
-          res.json({
-            msg: 'fail'
-          })
-        }
-
-        res.json({
-          msg: 'success'
-        });
-      })
-      console.log('Server is ready to take messages');
-    });
-  }
-}
+const mailerController = require('./mailer');
+console.log("Mailer", mailerController);
 
 const app = express();
-const mailer = new Mailer();
+const mailer = mailerController();
 
 app.use(bodyParser.json());
 
---
+++ api/mailer.js
@@ -0,0 +1,44 @@
+export default function () {
+  return new Mailer();
+}
+
+class Mailer {
+  constructor() {
+    const creds = {
+      USER: 'raul.vega.dv@gmail.com',
+      PASS: 'gudvhlqnlrexajoi'
+    };
+
+    const transport = {
+      host: 'smtp.gmail.com',
+      auth: {
+        user: creds.USER,
+        pass: creds.PASS
+      }
+    };
+
+    this.transporter = nodemailer.createTransport(transport);
+  }
+
+  sendEmail(mail) {
+    this.transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+      }
+
+      this.transporter.sendMail(mail, (err, data) => {
+        console.log('sending...');
+        if (err) {
+          res.json({
+            msg: 'fail'
+          })
+        }
+
+        res.json({
+          msg: 'success'
+        });
+      })
+      console.log('Server is ready to take messages');
+    });
+  }
+}
