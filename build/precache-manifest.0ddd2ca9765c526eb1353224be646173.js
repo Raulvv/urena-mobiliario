b1c36e807d35b03dad51a09837bdd74999e4e41a
Raul Vega <rvr_sge@mango.com>
2019-09-09 10:54:06 +0200

    Setting env for production

---
+++ build/precache-manifest.0ddd2ca9765c526eb1353224be646173.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8750a7ce88d1d8d60459",
+    "url": "/static/js/main.8750a7ce.chunk.js"
+  },
+  {
+    "revision": "cecc484360638d2b836b",
+    "url": "/static/js/1.cecc4843.chunk.js"
+  },
+  {
+    "revision": "8750a7ce88d1d8d60459",
+    "url": "/static/css/main.81e9ac86.chunk.css"
+  },
+  {
+    "revision": "cecc484360638d2b836b",
+    "url": "/static/css/1.cb8284c4.chunk.css"
+  },
+  {
+    "revision": "cf613fea196daa7a6f57ad0261e8b287",
+    "url": "/index.html"
+  }
+];
