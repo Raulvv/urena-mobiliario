0347436c4b2781e03fa60d4189f4b92265bbf70b
Raul Vega <rvr_sge@mango.com>
2019-10-13 22:12:46 +0200

    Preventing bad behovior on filter

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -8,31 +8,46 @@ export default class Filter extends Component {
                 <nav className="Filter-container">
                     <div
                         className="Filter-item"
-                        onClick={() => this.props.onFilter("Mesas", 3)}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            this.props.onFilter("Mesas", 3)
+                        }}
                     >
                         <a href="#"><span>Mesas</span></a>
                     </div>
                     <div
                         className="Filter-item"
-                        onClick={() => this.props.onFilter("Roperos", 1)}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            this.props.onFilter("Roperos", 1)
+                        }}
                     >
                         <a href="#"><span>Roperos</span></a>
                     </div>
                     <div
                         className="Filter-item"
-                        onClick={() => this.props.onFilter("Cajoneras", 2)}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            this.props.onFilter("Cajoneras", 2)
+                        }}
                     >
                         <a href="#"><span>Cajoneras</span></a>
                     </div>
                     <div
                         className="Filter-item"
-                        onClick={() => this.props.onFilter("Archivo", 2)}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            this.props.onFilter("Archivo", 2)
+                        }}
                     >
                         <a href="#"><span>Archivo</span></a>
                     </div>
                     <div
                         className="Filter-item"
-                        onClick={() => this.props.onFilter("Armarios", 3)}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            this.props.onFilter("Armarios", 3)
+                        }}
                     >
                         <a href="#"><span>Armarios</span></a>
                     </div>
